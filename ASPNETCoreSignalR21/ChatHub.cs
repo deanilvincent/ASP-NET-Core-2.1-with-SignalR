535231ea5cc75531e7729b073aa4ac4f7f41d0f9
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-06-03 23:23:54 +0800

    Add project files.

---
+++ ASPNETCoreSignalR21/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ASPNETCoreSignalR21
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("SendAction", "Name Here", "joined");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("SendAction", "Name Here", "left");
+        }
+
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("SendMessage", "Name Here", message);
+        }
+    }
+}
